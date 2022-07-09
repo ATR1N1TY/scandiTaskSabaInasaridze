eeb8e86619c100b72c7e39ab4f36b814c8674df2
unknown <saba4inasaridze@gmail.com>
2022-07-09 12:48:30 +0400

    Added current page indicator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Header from "./components/header";
+import ProductListPage from "./components/productListPage";
+import ProductCard from "./components/productCard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <div className="pageName">
+        <h1>Products</h1>
+      </div>
+
+      <ProductListPage>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </ProductListPage>
     </div>
   );
 }
